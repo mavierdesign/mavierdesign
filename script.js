7cc9bf6f1c95bf9af4e3949575d97f99df7a3f0c
Javier Maldonado García <javier.maldonado1129@uppuebla.edu.mx>
2025-05-12 15:03:57 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+      duration: 1000, // duración de la animación en milisegundos
+      once: true      // solo anima una vez al hacer scroll
+    });
+  });
+  
