91b2dde17ed03ef84a6dddfe34a77342ae076704
Javier Maldonado García <javier.maldonado1129@uppuebla.edu.mx>
2025-05-15 10:04:53 -0600

    actualizacion pagina

--- script.js
+++ script.js
@@ -12,4 +12,30 @@ document.addEventListener('DOMContentLoaded', function () {
       once: true      // solo anima una vez al hacer scroll
     });
   });
+
   
+ const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+
